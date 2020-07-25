96abc04242fcf36f3f9b74e39e010acb0455f595
Eric Mellow <ericmellow@gmail.com>
2020-07-25 17:12:57 -0600

    Add recipe styling

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -7,14 +7,14 @@
     />
     <div class="recipe-container">
       <div class="recipe" v-bind:key="recipe.id" v-for="recipe in recipes">
-      {{ recipe.title }}
+        {{ recipe.title }}
         <button v-on:click="openModal(recipe)">
           View
         </button>
         <button v-on:click="deleteRecipe(recipe)">
           Delete
         </button>
-    </div>
+      </div>
     </div>
     <Modal
       v-model="modalOpen"
@@ -67,20 +67,24 @@ export default Vue.extend({
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.recipe-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 80vh;
+  overflow-y: scroll;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.recipe {
+  box-shadow: 5px 5px 10px 0px black;
+  margin: 15px;
+  border-radius: 3px;
+  width: 70vw;
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
