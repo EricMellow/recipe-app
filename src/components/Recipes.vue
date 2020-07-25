9d1ffbdb42b616947c2f6d3187df453f13a79bf6
Eric Mellow <ericmellow@gmail.com>
2020-07-25 09:14:54 -0600

    Initial store setup

---
+++ src/components/Recipes.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="recipes">
+    <div v-bind:key="recipe.id" v-for="recipe in recipes">{{ recipe.title }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Recipes",
+  data() {
+    return {
+      recipes: this.$store.state.recipes
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    recipes: [{ id: 1, title: 'First Recipe', ingredients: 'Some food and stuff' }, { id: 2, title: 'Second Recipe', ingredients: 'Some food and stuff' }]
+  },
+  mutations: {
+    addRecipe (state, newRecipe) {
+      state.recipes = [newRecipe, ...state.recipes]
+    }
+  },
   actions: {},
   modules: {}
 });
